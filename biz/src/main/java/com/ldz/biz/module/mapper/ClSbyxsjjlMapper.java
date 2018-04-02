7fe45518514df2d245c1950d2aebdaaa4b99e383
liuzhihao <1969994698@qq.com>
2018-04-02 11:30:31 +0800

    新增查看历史轨迹接口 提交周报

--- biz/src/main/java/com/ldz/biz/module/mapper/ClSbyxsjjlMapper.java
+++ biz/src/main/java/com/ldz/biz/module/mapper/ClSbyxsjjlMapper.java
@@ -1,10 +1,13 @@
 package com.ldz.biz.module.mapper;
 
+import java.util.List;
+
+import com.ldz.biz.module.bean.gpsSJInfo;
 import com.ldz.biz.module.model.ClSbyxsjjl;
 
 import tk.mybatis.mapper.common.Mapper;
 
 public interface ClSbyxsjjlMapper extends Mapper<ClSbyxsjjl> {
 	
-	
+	List<ClSbyxsjjl> historyTrajectory(gpsSJInfo gpssjinfo);
 }
--- biz/src/main/java/com/ldz/biz/module/service/SbyxsjjlService.java
+++ biz/src/main/java/com/ldz/biz/module/service/SbyxsjjlService.java
@@ -2,8 +2,13 @@ package com.ldz.biz.module.service;
 
 import com.ldz.util.bean.ApiResponse;
 import com.ldz.sys.base.BaseService;
+
+import java.util.List;
+
+import com.ldz.biz.module.bean.gpsSJInfo;
 import com.ldz.biz.module.model.ClSbyxsjjl;
 
 public interface SbyxsjjlService extends BaseService<ClSbyxsjjl,String>{
     ApiResponse<String> saveEntity(ClSbyxsjjl entity);
+    ApiResponse<List<ClSbyxsjjl>> historyTrajectory(gpsSJInfo gpssjinfo);
 }
