c7cca9cce0aae920b9e3a5bcd33fc2ed0c735c75
liuzhihao <1969994698@qq.com>
2018-04-09 17:21:56 +0800

    新增weixin-java 依赖

---
+++ biz/src/main/java/com/ldz/biz/module/weixin/test.java
@@ -0,0 +1,15 @@
+package com.ldz.biz.module.weixin;
+
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpServiceImpl;
+
+public class test {
+
+	
+	public static void main(String[] args) {
+		
+		WxMpService wxMpService = new WxMpServiceImpl();
+	}
+	
+	
+}
