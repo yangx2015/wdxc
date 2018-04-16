d4b42a881c1d63850733ae86aff6d6fd5637bbe7
chenwei <libinbin@rhtop.cn>
2018-04-16 11:02:55 +0800

    test

--- web/pc/projectWD/src/views/whdx/VehicleScheduling/ScManage/Sc_SiteMaintenance/index.vue
+++ web/pc/projectWD/src/views/whdx/VehicleScheduling/ScManage/Sc_SiteMaintenance/index.vue
@@ -16,7 +16,7 @@
 						</div>
 						<div class="body-r-1 inputSty">
 							<DatePicker v-model="cjsjInRange" format="yyyy-MM-dd" type="daterange" placement="bottom-end" placeholder="请输时间" @on-keyup.enter="findMessList()" style="width: 220px"></DatePicker>
-							<Input v-model="findMess.zjhmLike" placeholder="请输入用户名" style="width: 200px" @on-keyup.enter="findMessList()"></Input>
+							<Input v-model="findMess.mcLike" placeholder="请输入用户名" style="width: 200px" @on-keyup.enter="findMessList()"></Input>
 						</div>
 						<div class="butevent">
 							<Button type="primary" @click="findMessList()">
@@ -162,7 +162,7 @@
                 cjsjInRange:[],
 				findMess: {
 					cjsjInRange:'',
-					zjhmLike: '',
+                    mcLike: '',
 					pageNum: 1,
 					pageSize: 5
 				}
@@ -202,7 +202,7 @@
                 })
 			},
         	findMessList(){
-        		alert('查询')
+        	    this.getmess();
         	},
         	AddDataList(){
                 this.choosedRow = null;
