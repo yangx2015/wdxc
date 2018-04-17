6580a44ab64f25b6a5e5a3928cfa411fc8139c14
chenwei <libinbin@rhtop.cn>
2018-04-17 15:42:05 +0800

    div

--- web/pc/projectWD/src/views/whdx/VehicleScheduling/AccidentManagement/index.vue
+++ web/pc/projectWD/src/views/whdx/VehicleScheduling/AccidentManagement/index.vue
@@ -11,7 +11,7 @@
 <template>
 	<div class="boxbackborder">
 		<Card>
-			<Row class="margin-top-30" style='background-color: #fff;position: relative;'>
+			<Row class="margin-top-10" style='background-color: #fff;position: relative;'>
 				<span class="tabPageTit">
     				<Icon type="ios-paper" size='30' color='#fff'></Icon>
     			</span>
