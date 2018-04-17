0612eb9628f0d02f84f4079c7e427c5b06b73da2
liuzhihao <1969994698@qq.com>
2018-04-17 14:52:57 +0800

    新增批量设置超速值接口

--- biz/src/main/java/com/ldz/biz/module/service/impl/CssdServiceImpl.java
+++ biz/src/main/java/com/ldz/biz/module/service/impl/CssdServiceImpl.java
@@ -1,69 +1,145 @@
 package com.ldz.biz.module.service.impl;
 
-import com.ldz.sys.exception.RuntimeCheck;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+
+import com.ldz.biz.module.bean.GpsInfo;
+import com.ldz.biz.module.mapper.ClClMapper;
+import com.ldz.biz.module.mapper.ClCssdMapper;
+import com.ldz.biz.module.model.ClCl;
+import com.ldz.biz.module.model.ClCssd;
+import com.ldz.biz.module.service.CssdService;
+import com.ldz.sys.base.BaseServiceImpl;
 import com.ldz.sys.base.LimitedCondition;
+import com.ldz.sys.exception.RuntimeCheck;
 import com.ldz.sys.model.SysJg;
 import com.ldz.sys.model.SysYh;
 import com.ldz.sys.service.JgService;
 import com.ldz.util.bean.ApiResponse;
-import com.ldz.sys.base.BaseServiceImpl;
-import com.ldz.biz.module.mapper.ClCssdMapper;
-import com.ldz.biz.module.model.ClCssd;
-import com.ldz.biz.module.service.CssdService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.ldz.util.commonUtil.HttpUtil;
+import com.ldz.util.commonUtil.JsonUtil;
+
 import tk.mybatis.mapper.common.Mapper;
 
-import java.util.Date;
-import java.util.List;
-
 @Service
-public class CssdServiceImpl extends BaseServiceImpl<ClCssd,String> implements CssdService{
-    @Autowired
-    private ClCssdMapper entityMapper;
-    @Autowired
-    private JgService jgService;
+public class CssdServiceImpl extends BaseServiceImpl<ClCssd, String> implements CssdService {
+	@Autowired
+	private ClCssdMapper entityMapper;
+	@Autowired
+	private JgService jgService;
+	@Autowired
+	private ClClMapper clclmapper;
 
-    @Override
-    protected Mapper<ClCssd> getBaseMapper() {
-        return entityMapper;
-    }
+	@Override
+	protected Mapper<ClCssd> getBaseMapper() {
+		return entityMapper;
+	}
 
-    @Override
-    protected Class<?> getEntityCls(){
-        return ClCssd.class;
-    }
+	@Override
+	protected Class<?> getEntityCls() {
+		return ClCssd.class;
+	}
 
-    @Override
-    public ApiResponse<String> saveEntity(ClCssd entity) {
-        SysYh user = getCurrentUser();
-        SysJg org = jgService.findByOrgCode(user.getJgdm());
-        Date now = new Date();
-        entity.setCjr(getOperateUser());
-        entity.setCjsj(now);
-        entity.setId(genId());
-        entity.setJgdm(user.getJgdm());
-        entity.setJgmc(org.getJgmc());
-        save(entity);
-        return ApiResponse.saveSuccess();
-    }
+	@Override
+	public ApiResponse<String> saveEntity(ClCssd entity) {
+		SysYh user = getCurrentUser();
+		SysJg org = jgService.findByOrgCode(user.getJgdm());
+		Date now = new Date();
+		entity.setCjr(getOperateUser());
+		entity.setCjsj(now);
+		entity.setId(genId());
+		entity.setJgdm(user.getJgdm());
+		entity.setJgmc(org.getJgmc());
+		save(entity);
+		return ApiResponse.saveSuccess();
+	}
 
-    @Override
-    public ApiResponse<String> updateEntity(ClCssd entity) {
-        ClCssd oldRecord = findById(entity.getId());
-        RuntimeCheck.ifNull(oldRecord,"未找到记录");
-        entity.setXgr(getOperateUser());
-        entity.setXgsj(new Date());
-        update(entity);
-        return ApiResponse.success();
-    }
+	@Override
+	public ApiResponse<String> updateEntity(ClCssd entity) {
+		ClCssd oldRecord = findById(entity.getId());
+		RuntimeCheck.ifNull(oldRecord, "未找到记录");
+		entity.setXgr(getOperateUser());
+		entity.setXgsj(new Date());
+		update(entity);
+		return ApiResponse.success();
+	}
 
-    @Override
-    public ApiResponse<List<ClCssd>> getByCx(String cx) {
-        // 自带机构筛选条件
-        LimitedCondition condition = new LimitedCondition(ClCssd.class);
-        condition.eq(ClCssd.InnerColumn.cx,cx);
-        List<ClCssd> cssds = findByCondition(condition);
-        return ApiResponse.success(cssds);
-    }
+	@Override
+	public ApiResponse<List<ClCssd>> getByCx(String cx) {
+		// 自带机构筛选条件
+		LimitedCondition condition = new LimitedCondition(ClCssd.class);
+		condition.eq(ClCssd.InnerColumn.cx, cx);
+		List<ClCssd> cssds = findByCondition(condition);
+		return ApiResponse.success(cssds);
+	}
+
+	@Override
+	public ApiResponse<String> setCssds(List<String> cphs, String csz) {
+		SysYh user = getCurrentUser();
+		SysJg org = jgService.findByOrgCode(user.getJgdm());
+		// 将车牌号,车辆信息缓存
+		List<ClCl> selectAll = clclmapper.selectAll();
+		Map<String, ClCl> clmap = selectAll.stream().collect(Collectors.toMap(ClCl::getCph, ClCl -> ClCl));
+
+		for (String cph : cphs) {
+			GpsInfo gpsInfo = new GpsInfo();
+			ClCssd cssd = new ClCssd();
+
+			ClCl clCl = clmap.get(cph);
+			gpsInfo.setDeviceId(clCl.getZdbh());
+			gpsInfo.setCmdType("01");
+			gpsInfo.setCmd(csz);
+			senZl(gpsInfo);
+
+			cssd.setCjr(getOperateUser());
+			cssd.setCjsj(new Date());
+			cssd.setCph(cph);
+			cssd.setId(genId());
+			cssd.setJgdm(user.getJgdm());
+			cssd.setJgmc(org.getJgmc());
+			cssd.setSdsx(Short.valueOf(csz));
+			insetAndUpdate(cssd);
+
+		}
+		return ApiResponse.success();
+	}
+
+	@Override
+	public void insetAndUpdate(ClCssd entity) {
+
+		boolean flag = ifExists("cph", entity.getCph());
+		if (flag == true) {
+			update(entity);
+		} else {
+			save(entity);
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public ApiResponse<String> senZl(GpsInfo info) {
+		String url = "http://47.98.39.45:8080/tic-server/api/push/carcmd";
+		String postEntity = JsonUtil.toJson(info);
+		String result = "";
+		ApiResponse<String> apiResponse = null;
+		try {
+			Map<String, String> postHeaders = new HashMap<String, String>();
+			postHeaders.put("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+			result = HttpUtil.postJson(url, postHeaders, postEntity);
+			apiResponse = (ApiResponse<String>) JsonUtil.toBean(result, ApiResponse.class);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return apiResponse;
+	}
+
 }
--- biz/src/main/java/com/ldz/biz/module/service/impl/XlServiceImpl.java
+++ biz/src/main/java/com/ldz/biz/module/service/impl/XlServiceImpl.java
@@ -1,5 +1,16 @@
 package com.ldz.biz.module.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.ldz.biz.module.mapper.ClXlMapper;
 import com.ldz.biz.module.mapper.ClXlzdMapper;
 import com.ldz.biz.module.model.ClXl;
@@ -13,105 +24,127 @@ import com.ldz.sys.model.SysYh;
 import com.ldz.sys.service.JgService;
 import com.ldz.util.bean.ApiResponse;
 import com.ldz.util.bean.SimpleCondition;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.ldz.util.gps.BaiduWebApi;
+import com.ldz.util.gps.bean.RouteMatrixBean;
+import com.ldz.util.gps.bean.RouteMatrixResult;
+
 import tk.mybatis.mapper.common.Mapper;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
-public class XlServiceImpl extends BaseServiceImpl<ClXl,String> implements XlService {
-    @Autowired
-    private ClXlMapper entityMapper;
-    @Autowired
-    private JgService jgService; 
-    @Autowired
-    private XlzdService xlzdService;
-    @Autowired
-    private XlService xlService;
-    @Autowired
-    private ClXlzdMapper xlzdMapper;
+public class XlServiceImpl extends BaseServiceImpl<ClXl, String> implements XlService {
+	@Autowired
+	private ClXlMapper entityMapper;
+	@Autowired
+	private JgService jgService;
+	@Autowired
+	private XlzdService xlzdService;
+	@Autowired
+	private XlService xlService;
+	@Autowired
+	private ClXlzdMapper xlzdMapper;
 
-    @Override
-    protected Mapper<ClXl> getBaseMapper() {
-        return entityMapper;
-    }
+	@Override
+	protected Mapper<ClXl> getBaseMapper() {
+		return entityMapper;
+	}
 
-    @Override
-    protected Class<?> getEntityCls(){
-        return ClXl.class;
-    }
+	@Override
+	protected Class<?> getEntityCls() {
+		return ClXl.class;
+	}
 
+	@Override
+	public ApiResponse<String> saveEntity(ClXl entity) {
+		SysYh user = getCurrentUser();
+		SysJg org = jgService.findByOrgCode(user.getJgdm());
+		Date now = new Date();
+		entity.setCjr(getOperateUser());
+		entity.setCjsj(now);
+		entity.setJgdm(user.getJgdm());
+		entity.setJgmc(org.getJgmc());
+		entity.setId(genId());
+		save(entity);
+		if (StringUtils.isNotBlank(entity.getZdIds())) {
+			List<String> stationIds = Arrays.asList(entity.getZdIds().split(","));
+			saveRouterStations(entity, stationIds);
+		}
+		return ApiResponse.saveSuccess();
+	}
 
-    @Override
-    public ApiResponse<String> saveEntity(ClXl entity) {
-    	 SysYh user = getCurrentUser();
-         SysJg org = jgService.findByOrgCode(user.getJgdm());
-         Date now = new Date();
-         entity.setCjr(getOperateUser());
-         entity.setCjsj(now);
-         entity.setJgdm(user.getJgdm());	
-         entity.setJgmc(org.getJgmc());
-         entity.setId(genId());
-         save(entity);
-        if (StringUtils.isNotBlank(entity.getZdIds())){
-            List<String> stationIds = Arrays.asList(entity.getZdIds().split(","));
-            saveRouterStations(entity,stationIds);
-        }
-        return ApiResponse.saveSuccess();
-    }
-
-    private void saveRouterStations(ClXl router,List<String> stationIds){
-        SimpleCondition condition = new SimpleCondition(ClXlzd.class);
-        condition.eq(ClXlzd.InnerColumn.xlId,router.getId());
-        xlzdMapper.deleteByExample(condition);
-
-        String operator = getOperateUser();
-        Date now = new Date();
-        int i = 0;
-        List<ClXlzd> xlzds = new ArrayList<>(stationIds.size());
-        for (String stationId : stationIds) {
-            ClXlzd xlzd = new ClXlzd();
-            xlzd.setCjr(operator);
-            xlzd.setCjsj(now);
-            xlzd.setFx(router.getYxfs());
-            xlzd.setXh(new Short(""+(++i)));
-            xlzd.setXlId(router.getId());
-            xlzd.setZdId(stationId);
-            xlzd.setId(genId());
-//            xlzd.setZt(Dict.CommonStatus.VALID.getCode());
-            xlzd.setZt("00");
-            xlzds.add(xlzd);
-        }
-        xlzdMapper.insertList(xlzds);
-    }
+	private void saveRouterStations(ClXl router, List<String> stationIds) {
+		SimpleCondition condition = new SimpleCondition(ClXlzd.class);
+		condition.eq(ClXlzd.InnerColumn.xlId, router.getId());
+		xlzdMapper.deleteByExample(condition);
+		
+         //获取站点信息
+		
+		
+		/*
+		String operator = getOperateUser();
+		Date now = new Date();
+		int i = 0;
+		List<ClXlzd> xlzds = new ArrayList<>(stationIds.size());
+		for (String stationId : stationIds) {
+			
+			ClXlzd xlzd = new ClXlzd();
+			
+			xlzd.setCjr(operator);
+			xlzd.setCjsj(now);
+			xlzd.setFx(router.getYxfs());
+			xlzd.setXh(new Short("" + (++i)));
+			xlzd.setXlId(router.getId());
+			xlzd.setZdId(stationId);
+			xlzd.setId(genId());
+			// xlzd.setZt(Dict.CommonStatus.VALID.getCode());
+			xlzd.setZt("00");
+			xlzds.add(xlzd);
+		}*/
+		
+		/*xlzdMapper.insertList(xlzds);*/
+	}
 
 	@Override
 	public ApiResponse<String> updateEntity(ClXl entity) {
 		ClXl findById = findById(entity.getId());
-	        RuntimeCheck.ifNull(findById,"未找到记录");
-	        entity.setXgr(getOperateUser());
-	        entity.setXgsj(new Date());
-	        update(entity);
+		RuntimeCheck.ifNull(findById, "未找到记录");
+		entity.setXgr(getOperateUser());
+		entity.setXgsj(new Date());
+		update(entity);
 
-        if (StringUtils.isNotBlank(entity.getZdIds())){
-            List<String> stationIds = Arrays.asList(entity.getZdIds().split(","));
-            saveRouterStations(entity,stationIds);
-        }
+		if (StringUtils.isNotBlank(entity.getZdIds())) {
+			List<String> stationIds = Arrays.asList(entity.getZdIds().split(","));
+			saveRouterStations(entity, stationIds);
+		}
 		return ApiResponse.success();
 	}
-    @Override
-    public List<ClXl> getByZdId(String zdId) {
-        List<ClXlzd> xlzds = xlzdService.findEq(ClXlzd.InnerColumn.zdId,zdId);
-        if (xlzds.size() == 0)return new ArrayList<>();
-        List<String> xlIds = xlzds.stream().map(ClXlzd::getXlId).collect(Collectors.toList());
-        return xlService.findIn(ClXl.InnerColumn.id,xlIds);
-    }
 
+	@Override
+	public List<ClXl> getByZdId(String zdId) {
+		List<ClXlzd> xlzds = xlzdService.findEq(ClXlzd.InnerColumn.zdId, zdId);
+		if (xlzds.size() == 0)
+			return new ArrayList<>();
+		List<String> xlIds = xlzds.stream().map(ClXlzd::getXlId).collect(Collectors.toList());
+		return xlService.findIn(ClXl.InnerColumn.id, xlIds);
+	}
 
+	public static void main(String[] args) {
+         
+		List<String> qidian = new ArrayList<>();
+        String q="30.5307693887,114.3244618270";
+        qidian.add(q);
+		List<String> zhongdian = new ArrayList<>();
+        String z="30.5582816319,114.2131314340";
+        zhongdian.add(z);
+		RouteMatrixBean routematrix = BaiduWebApi.routematrix(qidian, zhongdian, "driving");
+		System.out.println(routematrix);
+		List<RouteMatrixResult> result = routematrix.getResult();
+		System.out.println(result);
+		for (RouteMatrixResult routeMatrixResult : result) {
+			Map<String, String> duration = routeMatrixResult.getDuration();
+			for (Object o : duration.keySet()) {
+				System.out.println("key=" + o + " value=" + duration.get(o));
+			}
+		}
+
+	}
 }
