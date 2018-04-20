0d797f1d60670c53f999505d268901f9799c18ed
yaingx <udjpyud@163.com>
2018-04-20 13:32:16 +0800

    智能

--- biz/src/main/java/com/ldz/biz/module/service/impl/ZdglServiceImpl.java
+++ biz/src/main/java/com/ldz/biz/module/service/impl/ZdglServiceImpl.java
@@ -59,7 +59,7 @@ public class ZdglServiceImpl extends BaseServiceImpl<ClZdgl,String> implements Z
 
     @Override
     public ApiResponse<String> updateEntity(ClZdgl entity) {
-    	ClZdgl findById = findById(entity.getZdbh());
+//    	ClZdgl findById = findById(entity.getZdbh());
 //    	if (findById!=null) {
 //			return ApiResponse.fail("终端编号已存在");
 //		}
