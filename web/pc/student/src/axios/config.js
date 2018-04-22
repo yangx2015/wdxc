69f5b6c0648382c6a24bd25adc95f1885b26bdb6
zhaohu <965567396@qq.com>
2018-04-22 19:34:30 +0800

    qweqwe

--- web/pc/student/src/axios/config.js
+++ web/pc/student/src/axios/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	UPLOAD:'http://47.98.39.45:8080/biz/upload',
-    STATIC_PATH:'http://47.98.39.45:9092/',
-	VIDEO_PATH:'http://47.98.39.45:9091',
+	SWIPER:{
+		QUERTY:'/put/xl/getadsense'
+	}
 }
