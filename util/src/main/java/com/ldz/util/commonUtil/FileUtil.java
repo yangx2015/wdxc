58e03a8862e08f06d8f3c50203f74ebc2bf25c6f
wanggang <1085772106@qq.com>
2018-04-23 10:23:20 +0800

    优化文件上传 优化下发指令【任何指令】都检查设备是否在线 新增检测设备是否在线的接口：接口名称/api/push/checkOnlin/{deviceId}  请求方式GET

--- util/src/main/java/com/ldz/util/commonUtil/FileUtil.java
+++ util/src/main/java/com/ldz/util/commonUtil/FileUtil.java
@@ -2,6 +2,9 @@ package com.ldz.util.commonUtil;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import org.springframework.util.FileCopyUtils;
 
 /**
  * @since 2017/11/1
@@ -16,5 +19,26 @@ public class FileUtil {
         out.write(file);
         out.flush();
         out.close();
+    }
+    
+    
+    public static void uploadCopyFile(InputStream  f,String filePath, String fileName) throws Exception {
+        File targetFile = new File(filePath);
+        if(!targetFile.exists()){
+            targetFile.mkdirs();
+        }
+        FileOutputStream out = new FileOutputStream(filePath+fileName);
+        //File out = new File(filePath+fileName);
+        FileCopyUtils.copy(f,out);
+        try {
+        	
+        	out.flush();
+        	out.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		} 
+        //out.write(file);
+        //out.flush();
+        //out.close();
     }
 }
