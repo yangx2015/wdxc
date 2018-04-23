3f053e080f5095542845dedee7597c826bdc8664
yaingx <udjpyud@163.com>
2018-04-23 14:56:44 +0800

    微信模块修改

--- biz/src/main/java/com/ldz/biz/module/service/impl/ObdServiceImpl.java
+++ biz/src/main/java/com/ldz/biz/module/service/impl/ObdServiceImpl.java
@@ -5,12 +5,18 @@ import com.ldz.biz.module.service.ObdService;
 import com.ldz.sys.base.BaseServiceImpl;
 import com.ldz.util.bean.ApiResponse;
 import com.ldz.util.bean.GpsObdMessageBean;
+import com.ldz.util.bean.ObdFaultCodeBean;
 import com.ldz.util.redis.RedisTemplateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ObdServiceImpl extends BaseServiceImpl<ObdMessageBean,String> implements ObdService {
@@ -23,6 +29,10 @@ public class ObdServiceImpl extends BaseServiceImpl<ObdMessageBean,String> imple
     @Value("${gpsObdMessage-key:gpsObdMessage_}")
     private String gpsObdMessage;
 
+    @Value("${obdFaultCodeList-key:obdFaultCodeList_}")
+    private String obdFaultCodeListKey;
+
+
     @Override
     protected Mapper<ObdMessageBean> getBaseMapper() {
         return null;
@@ -30,11 +40,18 @@ public class ObdServiceImpl extends BaseServiceImpl<ObdMessageBean,String> imple
 
     public ApiResponse<Object> getObdTimely(String obdId){
         GpsObdMessageBean obds=new GpsObdMessageBean();
+        List<ObdFaultCodeBean> list=new ArrayList<>();
         try {
            obds=  (GpsObdMessageBean)redisDao.opsForValue().get(gpsObdMessage+obdId);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        }catch (Exception e){}
+
+       try {
+            list=redisDao.opsForList().range(obdFaultCodeListKey+obdId,0,-1);
+       }catch (Exception E){}
+
+        Map ret= new HashMap();
+        ret.put("gpsObdMessage",obds);
+        ret.put("obdFaultCode",list);
         return ApiResponse.success(obds);
     }
 }
