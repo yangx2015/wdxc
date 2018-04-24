7f1378794b55b7b98a20eb1a8e14f7983c1fe76d
wanggang <1085772106@qq.com>
2018-04-24 10:45:23 +0800

    新增批量上传gps

--- tic-server/src/main/java/com/ldz/ticserver/api/DeviceApiConteroller.java
+++ tic-server/src/main/java/com/ldz/ticserver/api/DeviceApiConteroller.java
@@ -1,5 +1,7 @@
 package com.ldz.ticserver.api;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,17 +36,37 @@ public class DeviceApiConteroller {
 	@RequestMapping("/gps")
 	public ApiResponse<String> postGpsData(@RequestBody RequestCommonParamsDto dto){
 		ApiResponse<String> ar = new ApiResponse<>();
-		System.err.println("请求了GPS上传的方法");
+		logger.debug("请求了GPS上传的方法");
 		bizApiService.pushData(dto);
 		
-		if(dto!=null && dto.getEventType()!=null && dto.getEventType().equals("60")){
-			redisDao.boundSetOps(Consts.CAR_ONLINE_KEY).remove(dto.getDeviceId()+Consts.CAR_SPLITE+dto.getChannelId());
-		}else{
+		//if(dto!=null && dto.getEventType()!=null && dto.getEventType().equals("60")){
+		//	redisDao.boundSetOps(Consts.CAR_ONLINE_KEY).remove(dto.getDeviceId()+Consts.CAR_SPLITE+dto.getChannelId());
+		//}else{
 			redisDao.boundSetOps(Consts.CAR_ONLINE_KEY).add(dto.getDeviceId()+Consts.CAR_SPLITE+dto.getChannelId());
-		}
+		//}
 		ar.setResult(dto.toString());
 		return ar;
 	}
+	
+	/**
+	 * 批量接收GPS离线坐标
+	 * @param dtos
+	 * @return
+	 */
+	@RequestMapping("/dbgps")
+	public ApiResponse<String> postGpsDataAll(@RequestBody List<RequestCommonParamsDto> dtos){
+		ApiResponse<String> ar = new ApiResponse<>();
+		logger.debug("请求了批量GPS上传的方法");
+		for (RequestCommonParamsDto requestCommonParamsDto : dtos) {
+			bizApiService.pushData(requestCommonParamsDto);
+		}
+		if(dtos!=null){
+			redisDao.boundSetOps(Consts.CAR_ONLINE_KEY).add(dtos.get(0).getDeviceId()+Consts.CAR_SPLITE+dtos.get(0).getChannelId());
+		}
+		//ar.setResult(dto.toString());
+		return ar;
+	}
+	
 	
 	/**
 	 * 接收上传的汽车速度信息数据
