abc2d8952538dc95b041f204ed6cb0c9e7ef0ec2
zhaohu <965567396@qq.com>
2018-04-24 17:01:40 +0800

    zzz

--- web/pc/projectWD/src/views/whdx/VehicleScheduling/vehicle-management/comp/newmes.vue
+++ web/pc/projectWD/src/views/whdx/VehicleScheduling/vehicle-management/comp/newmes.vue
@@ -124,11 +124,12 @@
 			}
 		},
 		created(){
+			console.log('详细信息',this.mess)
             if(!this.messType){
                 this.operate = '编辑';
                 this.editMode = true;
                 this.addmess = this.mess
-                console.log('数据传递',this.derMess)
+//              console.log('数据传递',this.derMess)
             }
             this.getDrivers();
 			this.getDict();
@@ -141,7 +142,10 @@
                 v.$http.post(configApi.ZDGL.SXQUERY).then((res) =>{
                     if(res.code===200){
                     	console.log('终端数据',res)
-                        this.deviceList = res.result;
+                    	if(res.result){
+                    		v.deviceList = res.result;
+                    	}
+                        v.deviceList.push({'zdbh':this.mess.zdbh})
                     }
                 })
 			},
