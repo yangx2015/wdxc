d53e7ef722d8af27cd9b5df30b8f02374bf0dcd4
zhaohu <965567396@qq.com>
2018-04-26 17:07:10 +0800

    qq

--- web/pc/projectWD/src/views/whdx/VehicleScheduling/vehicle-management/comp/newmes.vue
+++ web/pc/projectWD/src/views/whdx/VehicleScheduling/vehicle-management/comp/newmes.vue
@@ -93,7 +93,7 @@
                     cx:'',
                     dl:'',
                     sjxm:'',
-                    zt:'',
+                    zt:'00',
                     zdbh:'',
                     sjId:'',
                     obdCode:''
