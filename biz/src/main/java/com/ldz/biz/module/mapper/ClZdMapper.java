13e32e1a6a7c255cb6f651ba6c80dda6784c494e
chenwei <libinbin@rhtop.cn>
2018-05-03 14:41:26 +0800

    二级缓存

--- biz/src/main/java/com/ldz/biz/module/mapper/ClZdMapper.java
+++ biz/src/main/java/com/ldz/biz/module/mapper/ClZdMapper.java
@@ -2,14 +2,14 @@ package com.ldz.biz.module.mapper;
 
 import com.ldz.biz.module.bean.DdClModel;
 import com.ldz.biz.module.model.ClZd;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import com.ldz.util.cache.MybatisRedisCache;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.cache.decorators.FifoCache;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
+@CacheNamespace(implementation=MybatisRedisCache.class, eviction=FifoCache.class)
 public interface ClZdMapper extends Mapper<ClZd> {
 
     @Select("SELECT Z.FW AS FW,Z.ID AS ID,Z.MC AS MC,(SELECT COUNT(1) FROM CL_CLYXJL JL WHERE JL.ZDBH=Z.ID) AS CLSL FROM CL_XLZD T,CL_ZD Z WHERE T.ZD_ID=Z.ID AND T.ZT='00' AND Z.ZT ='00' AND  T.XL_ID= #{id} ORDER BY T.XH ASC")
--- sys/src/main/java/com/ldz/sys/mapper/SysJsGnMapper.java
+++ sys/src/main/java/com/ldz/sys/mapper/SysJsGnMapper.java
@@ -1,8 +1,12 @@
 package com.ldz.sys.mapper;
 
 import com.ldz.sys.model.SysJsGn;
+import com.ldz.util.cache.MybatisRedisCache;
 import com.ldz.util.mapperprovider.OracleInsertListMapper;
+import org.apache.ibatis.annotations.CacheNamespace;
+import org.apache.ibatis.cache.decorators.FifoCache;
 import tk.mybatis.mapper.common.Mapper;
 
+@CacheNamespace(implementation=MybatisRedisCache.class, eviction=FifoCache.class)
 public interface SysJsGnMapper extends Mapper<SysJsGn>,OracleInsertListMapper<SysJsGn> {
 }
