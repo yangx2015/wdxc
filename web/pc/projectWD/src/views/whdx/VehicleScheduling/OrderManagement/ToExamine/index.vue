3b9c133e335c4f143ce263f8ca09590f68eb6e21
chenwei <libinbin@rhtop.cn>
2018-05-04 15:23:21 +0800

    router

--- web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/ToExamine/index.vue
+++ web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/ToExamine/index.vue
@@ -6,30 +6,27 @@
 <template>
     <div class="box">
 		<Card>
-			<div class="tit">
-				<Row class="margin-top-10">
-					<Col span="6">
-						<DatePicker v-model="datetime" type="datetime" placeholder="请输时间" style="width: 220px" @on-change="changeTime"></DatePicker>
-					</Col>
-					<Col span="6">
-						<Input v-model="findMess.like_CarNumber" placeholder="..." style="width: 200px" @on-change="findMessList"></Input>
-					</Col>
-					<Col span="6">
-						<Input v-model="findMess.like_ScName" placeholder="..." style="width: 200px" @on-change="findMessList"></Input>
-					</Col>
-					<Col span="6" style="text-align: right;">
-						<Button type="primary" @click="findMessList()">查询</Button>
-					</Col>
-				</Row>
-				<Row class="margin-top-10" style='background-color: #fff;position: relative;'>
+			<Row class="margin-top-10" style='background-color: #fff;position: relative;'>
     			<span class="tabPageTit">
     				<Icon type="ios-paper" size='30' color='#fff'></Icon>
     			</span>
-					<div style="height: 45px;line-height: 45px;">
-						<span style="margin-left: 60px;font-size: 24px;">订单审核</span>
+				<div style="height: 45px;line-height: 45px;">
+					<div class="margin-top-10 box-row">
+						<div class="titmess">
+							<span>订单审核</span>
+						</div>
+						<div class="body-r-1 inputSty">
+							<DatePicker v-model="datetime" type="datetime" placeholder="请输时间" style="width: 220px" @on-change="changeTime"></DatePicker>
+						</div>
+						<div class="butevent">
+							<Button type="primary" @click="findMessList()">
+								<Icon type="search"></Icon>
+								<!--查询-->
+							</Button>
+						</div>
 					</div>
-				</Row>
-			</div>
+				</div>
+			</Row>
 			<div class="body">
 				<Row>
 					<Table
@@ -154,54 +151,6 @@
                     }
                 ],
                 tableData: [
-	                {
-	                	id:'201801020903000001',
-	                	jgmc:'计算机学院',
-	                	ck:'毛毛',
-	                	cklxdh:'13174174110',
-	                	hcdz:'武汉大学正门',
-	                	mdd:'武汉火车站'
-	                },
-	                {
-	                	id:'201801020903000001',
-	                	jgmc:'计算机学院',
-	                	ck:'毛毛',
-	                	cklxdh:'13174174110',
-	                	hcdz:'武汉大学正门',
-	                	mdd:'武汉火车站'
-	                },
-	                {
-	                	id:'201801020903000001',
-	                	jgmc:'计算机学院',
-	                	ck:'毛毛',
-	                	cklxdh:'13174174110',
-	                	hcdz:'武汉大学正门',
-	                	mdd:'武汉火车站'
-	                },
-	                {
-	                	id:'201801020903000001',
-	                	jgmc:'计算机学院',
-	                	ck:'毛毛',
-	                	cklxdh:'13174174110',
-	                	hcdz:'武汉大学正门',
-	                	mdd:'武汉火车站'
-	                },
-	                {
-	                	id:'201801020903000001',
-	                	jgmc:'计算机学院',
-	                	ck:'毛毛',
-	                	cklxdh:'13174174110',
-	                	hcdz:'武汉大学正门',
-	                	mdd:'武汉火车站'
-	                },
-	                {
-	                	id:'201801020903000001',
-	                	jgmc:'计算机学院',
-	                	ck:'毛毛',
-	                	cklxdh:'13174174110',
-	                	hcdz:'武汉大学正门',
-	                	mdd:'武汉火车站'
-	                }
                 ],
                 //form表单
                 formTop: {
--- web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/dz_Confirm/index.vue
+++ web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/dz_Confirm/index.vue
@@ -5,47 +5,46 @@
 <template>
 	<div class="box">
 		<component :is="componentName"></component>
-		<div class="tit">
-			<Row class="margin-top-10">
-				<Col span="6">
-					<DatePicker v-model="datetime" type="datetime" placeholder="请输时间" style="width: 220px" @on-change="changeTime"></DatePicker>
-				</Col>
-				<Col span="6">
-					<Input v-model="findMess.like_CarNumber" placeholder="..." style="width: 200px" @on-change="findMessList"></Input>
-				</Col>
-				<Col span="6">
-					<Input v-model="findMess.like_ScName" placeholder="..." style="width: 200px" @on-change="findMessList"></Input>
-				</Col>
-				<Col span="6" style="text-align: right;">
-					<Button type="primary" @click="findMessList()">查询</Button>
-				</Col>
-			</Row>
+		<Card>
 			<Row class="margin-top-10" style='background-color: #fff;position: relative;'>
     			<span class="tabPageTit">
     				<Icon type="ios-paper" size='30' color='#fff'></Icon>
     			</span>
 				<div style="height: 45px;line-height: 45px;">
-					<span style="margin-left: 60px;font-size: 24px;">订单确认</span>
+					<div class="margin-top-10 box-row">
+						<div class="titmess">
+							<span>订单确认</span>
+						</div>
+						<div class="body-r-1 inputSty">
+							<DatePicker v-model="datetime" type="datetime" placeholder="请输时间" style="width: 220px" @on-change="changeTime"></DatePicker>
+						</div>
+						<div class="butevent">
+							<Button type="primary" @click="findMessList()">
+								<Icon type="search"></Icon>
+								<!--查询-->
+							</Button>
+						</div>
+					</div>
 				</div>
 			</Row>
-		</div>
-		<div class="body">
-			<Row>
-				<Table
-						:row-class-name="rowClassName"
-						:columns="columns10"
-						:data="data9"></Table>
-			</Row>
-			<Row class="margin-top-10" style="text-align: right;">
-				<Page
-						:total=pageTotal
-						:current=page.pageNum
-						:page-size=page.pageSize
-						show-total
-						show-elevator
-						@on-change='pageChange'></Page>
-			</Row>
-		</div>
+			<div class="body">
+				<Row>
+					<Table
+							:row-class-name="rowClassName"
+							:columns="columns10"
+							:data="data9"></Table>
+				</Row>
+				<Row class="margin-top-10" style="text-align: right;">
+					<Page
+							:total=pageTotal
+							:current=page.pageNum
+							:page-size=page.pageSize
+							show-total
+							show-elevator
+							@on-change='pageChange'></Page>
+				</Row>
+			</div>
+		</Card>
 		<Modal
 				v-model="showModal"
 				width='800'
@@ -168,66 +167,6 @@
                     },
                 ],
                 data9: [
-                    {
-                        jgmc:'信息学院',//用车单位
-                        ck: '毛毛',//用车人
-                        cklxdh: '15113131414',//用车人电话
-                        zws: '45人座位',//车型--按座位数分
-                        BillModel: '单程',//单据类型--单程--往返
-                        mileage: '30公里',//行车里程
-                        unit: '15元/公里',//里程单价
-                        mileageMoney: '450元',//里程总金额
-                        Etc: '20分钟',//等时
-                        RoadToll:'10元',//过路费
-                        BridgeFee:'15元',//过桥费
-                        mess:'用车事由',//用车事由
-                        sjxm:'王二毛',//司机姓名
-                        DriverPhone:'13212121212',//司机电话
-                        WaitingPlace:'武汉大学正门',//候车地点
-                        Destination:'武汉火车站',//目的地
-                        startTime:'2017-01-02 08:00:00',//出发时间
-                        addMoney:'470元',//费用合计
-                    },
-                    {
-                        jgmc:'信息学院',
-                        ck: '毛毛',
-                        cklxdh: '15113131414',
-                        zws: '45人座位',
-                        BillModel: '单程',
-                        mileage: '30公里',
-                        unit: '15元/公里',
-                        mileageMoney: '450元',
-                        Etc: '20分钟',
-                        RoadToll:'10元',
-                        BridgeFee:'15元',
-                        mess:'用车事由',
-                        sjxm:'王二毛',
-                        DriverPhone:'13212121212',
-                        WaitingPlace:'武汉大学正门',
-                        Destination:'武汉火车站',
-                        startTime:'2017-01-02 08:00:00',
-                        addMoney:'470元',
-                    },
-                    {
-                        jgmc:'信息学院',
-                        ck: '毛毛',
-                        cklxdh: '15113131414',
-                        zws: '45人座位',
-                        BillModel: '单程',
-                        mileage: '30公里',
-                        unit: '15元/公里',
-                        mileageMoney: '450元',
-                        Etc: '20分钟',
-                        RoadToll:'10元',
-                        BridgeFee:'15元',
-                        mess:'用车事由',
-                        sjxm:'王二毛',
-                        DriverPhone:'13212121212',
-                        WaitingPlace:'武汉大学正门',
-                        Destination:'武汉火车站',
-                        startTime:'2017-01-02 08:00:00',
-                        addMoney:'470元',
-                    }
                 ]
             }
         },
