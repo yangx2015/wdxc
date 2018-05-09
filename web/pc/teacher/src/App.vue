86b8d4b0fb43b010fdf300da0d1666dab55eca22
zhaohu <965567396@qq.com>
2018-05-09 14:42:13 +0800

    Merge branch 'master' of http://47.98.39.45:9000/r/wdxc
    # Conflicts:
    #	web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/Establish/index.vue

---
+++ web/pc/teacher/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app" style="height: 100%;background-color: #00a050">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  @import "//at.alicdn.com/t/font_605313_dytag0ll15eqxgvi.css";
+</style>
