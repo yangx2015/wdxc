47be67409b943989c4c99ba4630b337d8cd192d1
yaingx <udjpyud@163.com>
2018-05-14 10:02:46 +0800

    临时单位，在删除单位时，同步删除该单位下的所有车辆

--- biz/src/main/java/com/ldz/biz/module/service/impl/LsdwServiceImpl.java
+++ biz/src/main/java/com/ldz/biz/module/service/impl/LsdwServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ldz.biz.module.service.impl;
 
+import com.ldz.biz.module.model.ClLsc;
+import com.ldz.biz.module.service.LscService;
 import com.ldz.util.bean.ApiResponse;
 import com.ldz.sys.base.BaseServiceImpl;
 import com.ldz.biz.module.mapper.ClLsdwMapper;
@@ -15,7 +17,8 @@ import java.util.Date;
 public class LsdwServiceImpl extends BaseServiceImpl<ClLsdw,String> implements LsdwService{
     @Autowired
     private ClLsdwMapper entityMapper;
-
+    @Autowired
+    private LscService lscService;
     @Override
     protected Mapper<ClLsdw> getBaseMapper() {
         return entityMapper;
@@ -34,5 +37,21 @@ public class LsdwServiceImpl extends BaseServiceImpl<ClLsdw,String> implements L
         entity.setDjcs(new Short("0"));
         save(entity);
         return ApiResponse.saveSuccess();
+    }
+    /**
+     * 临时单位删除
+     * @param id
+     * @return
+     */
+    @Override
+    public ApiResponse<String> delUnit(String id){
+//        1、删除一个临时单位
+       this.remove(id);
+//        2、删除该单位下所有的车辆
+        ClLsc lsc=new ClLsc();
+        lsc.setLsdwId(id);
+        lscService.remove(lsc);
+
+       return ApiResponse.saveSuccess();
     }
 }
