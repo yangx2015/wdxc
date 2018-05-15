fc9b3bef3e7585e29b772ff4888067c1956a2c3d
zhaohu <965567396@qq.com>
2018-05-15 11:16:28 +0800

    Merge branch 'master' of http://47.98.39.45:9000/r/wdxc
    # Conflicts:
    #	web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/Establish/index.vue

--- web/pc/projectWD/src/views/home/compEcharts/comp/csMessbar.vue
+++ web/pc/projectWD/src/views/home/compEcharts/comp/csMessbar.vue
@@ -8,7 +8,6 @@
 		       width="80%"
 		       :mask-closable=false>
 			<div :style="{height: boxHeight}">
-					{{ech.time}}{{ech.cph}}
 				<div style="width:100%;height:100%;position: relative;">
 					<div style="width:100%;height:100%;" :id="Eid"></div>
 					<div v-if="SpinShow" style="width:100%;height:100%;position: absolute;top: 0;left:0;z-index: 100;">
@@ -117,7 +116,7 @@
                                 }
                             },
                             barWidth: '60%',
-                            data: this.result.list
+                            data: this.result.count
                         }],
                         color: ["#6495ed", "#da70d6", "#ff69b4", "#ba55d3", "#cd5c5c", "#ffa500", "#40e0d0", "#1e90ff", "#ff6347", "#7b68ee", "#00fa9a", "#ffd700", "#6699FF", "#ff6666", "#3cb371", "#b8860b", "#30e0e0"]
                     };
