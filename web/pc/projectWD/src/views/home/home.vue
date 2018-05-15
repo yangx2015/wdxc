25838f290b8c270e3fb02da2fd8c0df1ab970465
chenwei <libinbin@rhtop.cn>
2018-05-15 11:26:19 +0800

    图表

--- web/pc/projectWD/src/views/home/home.vue
+++ web/pc/projectWD/src/views/home/home.vue
@@ -18,31 +18,20 @@
 </style>
 <template>
 	<div class="box" style="height: 100%;background:#fff">
-		<!--<div class="body" style="position: relative">-->
-			<!--<span style="position: absolute;top: 45%;left: 50%;transform: translate(-50%,-50%);font-size: 26px;text-align: center;color: #a4a4a4">-->
-				<!--<h1>-->
-					<!--欢迎登录-->
-				<!--</h1>-->
-				<!--<h1>-->
-					<!--武汉大学车辆管理信息平台-->
-				<!--</h1>-->
-			<!--</span>-->
-		<!--</div>-->
-		<component
-			:is="compName"></component>
-		<div class="homeE" style="padding: 5px 3px;">
+		<component :is="compName"></component>
+		<div  v-if="showChart" class="homeE" style="padding: 5px 3px;">
 			<Row :gutter="8" class="margin-bottom-10 indexCarType">
 				<Col :xs="24" :sm="12" :md="6" :style="{marginBottom: '10px'}">
-				<infor-card id-name="user_created_count" :end-val="count.createUser" iconType="planet" color="#2d8cf0" intro-text="设备总数"></infor-card>
+					<infor-card id-name="user_created_count" :end-val="count.total" iconSrc="/static/icon/device.png" color="#2d8cf0" intro-text="设备总数"></infor-card>
 				</Col>
 				<Col :xs="24" :sm="12" :md="6" :style="{marginBottom: '10px'}">
-				<infor-card id-name="visit_count" :end-val="count.visit" iconType="social-twitter" color="#64d572" :iconSize="50" intro-text="设备在线数量"></infor-card>
+					<infor-card id-name="visit_count" :end-val="count.online"  iconSrc="/static/icon/device_online.png" color="#64d572" :iconSize="50" intro-text="设备在线数量"></infor-card>
 				</Col>
 				<Col :xs="24" :sm="12" :md="6" :style="{marginBottom: '10px'}">
-				<infor-card id-name="collection_count" :end-val="count.collection" iconType="social-chrome" color="#ffd572" intro-text="设备熄火数量"></infor-card>
+					<infor-card id-name="collection_count" :end-val="count.stop" iconSrc="/static/icon/device_stop.png" color="#ffd572" intro-text="设备熄火数量"></infor-card>
 				</Col>
 				<Col :xs="24" :sm="12" :md="6" :style="{marginBottom: '10px'}">
-				<infor-card id-name="transfer_count" :end-val="count.transfer" iconType="android-warning" color="#f25e43" intro-text="设备离线数量"></infor-card>
+					<infor-card id-name="transfer_count" :end-val="count.offline" iconSrc="/static/icon/device_offline.png" color="#f25e43" intro-text="设备离线数量"></infor-card>
 				</Col>
 			</Row>
 			<Row :gutter="12" class="margin-bottom-15">
@@ -104,6 +93,16 @@
 				</Col>
 			</Row>
 		</div>
+		<div v-else class="body" style="position: relative">
+			<span style="position: absolute;top: 45%;left: 50%;transform: translate(-50%,-50%);font-size: 26px;text-align: center;color: #a4a4a4">
+				<h1>
+					欢迎登录
+				</h1>
+				<h1>
+					武汉大学车辆管理信息平台
+				</h1>
+			</span>
+		</div>
 	</div>
 </template>
 
@@ -118,7 +117,7 @@
 	import risk from './compEcharts/riskRecord.vue'
 
 
-        import csMessbar from './compEcharts/comp/csMessbar'
+	import csMessbar from './compEcharts/comp/csMessbar'
 	export default {
 		name: 'home',
 		components: {
@@ -128,12 +127,14 @@
 		},
 		data() {
 			return {
-                		compName:'',
+                userType:'',
+                showChart:false,
+				compName:'',
 				count: {
-					createUser: 496,
-					visit: 400,
-					collection: 90,
-					transfer: 6
+                    total: 0,
+                    online: 0,
+                    stop: 0,
+                    offline: 0
 				},
 			};
 		},
@@ -143,15 +144,33 @@
 			}
 		    },
 		    watch:{
-			echData:function (n,o) {
-				this.compName = 'csMessbar'
-			}
+				echData:function (n,o) {
+					this.compName = 'csMessbar'
+				}
 		    },
 		created() {
 			this.$store.commit('setCurrentPath', [{
 				title: '首页'
 			}])
+            let userInfoJson = sessionStorage.getItem("userInfo");
+            let userInfo = JSON.parse(userInfoJson);
+            this.userType = userInfo.type;
+            if (this.userType == 'su' || this.userType == '00'){
+                this.showChart = true;
+                this.getDeviceCount();
+            }
 		},
-		methods: {}
+		methods: {
+            getDeviceCount(){
+                this.$http.get(this.apis.CHART_DATA.zdcx).then((res)=>{
+                    if (res.code == 200 && res.result){
+                        this.count.total = parseInt(res.result['设备总数'])
+                        this.count.online = parseInt(res.result['设备在线数量'])
+                        this.count.stop = parseInt(res.result['设备熄火数量'])
+                        this.count.offline = parseInt(res.result['设备离线数量'])
+                    }
+                })
+            }
+		}
 	};
 </script>
