4b75e3be46a6b65ef6b13e1bac3a7698ac650783
zhaohu <965567396@qq.com>
2018-05-15 11:15:25 +0800

    Merge branch 'master' of http://47.98.39.45:9000/r/wdxc
    # Conflicts:
    #	web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/Establish/index.vue

--- web/pc/projectWD/src/views/home/compEcharts/comp/csMessbar.vue
+++ web/pc/projectWD/src/views/home/compEcharts/comp/csMessbar.vue
@@ -99,7 +99,7 @@
                         },
                         xAxis: [{
                             type: 'category',
-                            data: this.result.carNumbers,
+                            data: this.result.list,
                             axisTick: {
                                 alignWithLabel: true
                             }
@@ -117,7 +117,7 @@
                                 }
                             },
                             barWidth: '60%',
-                            data: this.result.overSpeedMap.yAxis
+                            data: this.result.list
                         }],
                         color: ["#6495ed", "#da70d6", "#ff69b4", "#ba55d3", "#cd5c5c", "#ffa500", "#40e0d0", "#1e90ff", "#ff6347", "#7b68ee", "#00fa9a", "#ffd700", "#6699FF", "#ff6666", "#3cb371", "#b8860b", "#30e0e0"]
                     };
