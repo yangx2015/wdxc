77f7d36e5d6a9194d16240d91a0f457f8854c6a2
slu <384920400@qq.com>
2018-05-19 11:20:39 +0800

    添加鹰眼模型

---
+++ util/src/main/java/com/ldz/util/bean/TrackPoint.java
@@ -0,0 +1,135 @@
+package com.ldz.util.bean;
+
+/**
+ * 鹰眼轨迹模型
+ */
+public class TrackPoint {
+
+
+    private String ak;  //2pVOrCuBldNDOgDtwaYSP8gpQ2VQdZY9
+    private String service_id;  //200383
+    private String entity_name; //CWB201805181744
+    private String latitude;    //30.5427744502
+    private String longitude;   //114.2618477088
+    private String loc_time;    //1526637383
+    private String coord_type_input;    //wgs84
+    private String speed;   //30.25
+    private String direction;   //12
+    private String height;  //113.76
+    private String radius;  //3
+    private String _object_key; //liu_201805181744
+    private String city;    //hubei
+    private String province;    //wuhan
+
+    public String getAk() {
+        return ak;
+    }
+
+    public void setAk(String ak) {
+        this.ak = ak;
+    }
+
+    public String getService_id() {
+        return service_id;
+    }
+
+    public void setService_id(String service_id) {
+        this.service_id = service_id;
+    }
+
+    public String getEntity_name() {
+        return entity_name;
+    }
+
+    public void setEntity_name(String entity_name) {
+        this.entity_name = entity_name;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLoc_time() {
+        return loc_time;
+    }
+
+    public void setLoc_time(String loc_time) {
+        this.loc_time = loc_time;
+    }
+
+    public String getCoord_type_input() {
+        return coord_type_input;
+    }
+
+    public void setCoord_type_input(String coord_type_input) {
+        this.coord_type_input = coord_type_input;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getRadius() {
+        return radius;
+    }
+
+    public void setRadius(String radius) {
+        this.radius = radius;
+    }
+
+    public String get_object_key() {
+        return _object_key;
+    }
+
+    public void set_object_key(String _object_key) {
+        this._object_key = _object_key;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+}
---
+++ util/src/main/java/com/ldz/util/bean/YyEntity.java
@@ -0,0 +1,67 @@
+package com.ldz.util.bean;
+
+/**
+ * 鹰眼设备 entity
+ */
+public class YyEntity {
+
+    private String ak;
+
+    private String service_id;
+
+    private String entity_name;
+
+    private String entity_desc;
+
+    private String city;
+
+    private String district;
+
+    public String getAk() {
+        return ak;
+    }
+
+    public void setAk(String ak) {
+        this.ak = ak;
+    }
+
+    public String getService_id() {
+        return service_id;
+    }
+
+    public void setService_id(String service_id) {
+        this.service_id = service_id;
+    }
+
+    public String getEntity_name() {
+        return entity_name;
+    }
+
+    public void setEntity_name(String entity_name) {
+        this.entity_name = entity_name;
+    }
+
+    public String getEntity_desc() {
+        return entity_desc;
+    }
+
+    public void setEntity_desc(String entity_desc) {
+        this.entity_desc = entity_desc;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+}
