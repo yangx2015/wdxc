6e518f16218f5a2fef53ce3faec783c0d4fe2c98
slu <384920400@qq.com>
2018-05-19 20:28:45 +0800

    Merge branch 'master' of http://47.98.39.45:9000/r/wdxc
    # Conflicts:
    #	util/src/main/java/com/ldz/util/yingyan/GuiJIApi.java

--- util/src/main/java/com/ldz/util/bean/TrackPoints.java
+++ util/src/main/java/com/ldz/util/bean/TrackPoints.java
@@ -1,35 +1,20 @@
 package com.ldz.util.bean;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class TrackPoints {
 
     private String ak;
 
-    private String service_id;
+    private int service_id;
 
     private String point_list;
 
 
-    public String getAk() {
-        return ak;
-    }
 
-    public void setAk(String ak) {
-        this.ak = ak;
-    }
-
-    public String getService_id() {
-        return service_id;
-    }
-
-    public void setService_id(String service_id) {
-        this.service_id = service_id;
-    }
-
-    public String getPoint_list() {
-        return point_list;
-    }
-
-    public void setPoint_list(String point_list) {
-        this.point_list = point_list;
-    }
 }
--- util/src/main/java/com/ldz/util/yingyan/GuiJIApi.java
+++ util/src/main/java/com/ldz/util/yingyan/GuiJIApi.java
@@ -7,22 +7,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.ldz.util.bean.AddPointResponse;
-import com.ldz.util.bean.TrackPoint;
-import com.ldz.util.bean.TrackPoints;
+import com.ldz.util.bean.*;
 import org.apache.commons.lang.StringUtils;
 
-import com.ldz.util.bean.YingyanResponse;
-import com.ldz.util.bean.YyEntity;
 import com.ldz.util.commonUtil.DateUtils;
 import com.ldz.util.commonUtil.HttpUtil;
 import com.ldz.util.commonUtil.JsonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class GuiJIApi {
+
+	@Autowired
+	private static  RestTemplate restTemplate ;
   
     private final static String AK = "2pVOrCuBldNDOgDtwaYSP8gpQ2VQdZY9";
 
-    private final static String SERVICE_ID = "200383";
+    private final static int SERVICE_ID = 200383;
 
     private final static  String  saveEntityuRL="http://yingyan.baidu.com/api/v3/entity/add";
   
@@ -36,13 +39,15 @@ public class GuiJIApi {
 
 
 	private final static  String addPointURL = "http://yingyan.baidu.com/api/v3/track/addpoint";
+
+	private final static String getPointsURL = "http://yingyan.baidu.com/api/v3/track/gettrack";
     
     public static YingyanResponse changeEntity(YyEntity entity,String url){
     	
     	Map<String, String> beanmap = new HashMap<>();
     	
     	beanmap.put("ak", entity.getAk());
-    	beanmap.put("service_id", entity.getService_id());
+    	beanmap.put("service_id", entity.getService_id() +"");
     	beanmap.put("entity_name", entity.getEntity_name());
     	if (StringUtils.isNotEmpty(entity.getEntity_desc())) {
     		beanmap.put("entity_desc", entity.getEntity_desc());
@@ -65,7 +70,7 @@ public class GuiJIApi {
 		Map<String, String> beanmap = new HashMap<>();
 
 		beanmap.put("ak", entity.getAk());
-		beanmap.put("service_id", entity.getService_id());
+		beanmap.put("service_id", entity.getService_id()+"");
 		beanmap.put("point_list", entity.getPoint_list());
 		String postJson = null;
 		try {
@@ -86,7 +91,7 @@ public class GuiJIApi {
 		Map<String, String> beanmap = new HashMap<>();
 
 		beanmap.put("ak", entity.getAk());
-		beanmap.put("service_id", entity.getService_id());
+		beanmap.put("service_id", entity.getService_id() +"");
 		beanmap.put("entity_name",entity.getEntity_name());
 		beanmap.put("coord_type_input",entity.getCoord_type_input());
 		beanmap.put("latitude",entity.getLatitude()+"");
@@ -106,6 +111,38 @@ public class GuiJIApi {
 
 	}
 
+	public static TrackPointsForReturn getPoints(TrackJiuPian entity,String url) throws ParseException {
+
+		Map<String, String> beanmap = new HashMap<>();
+
+		beanmap.put("ak", entity.getAk());
+		beanmap.put("service_id", entity.getService_id() +"");
+		beanmap.put("entity_name",entity.getEntity_name());
+		Date date = DateUtils.getDate("2018-05-17 00:00:00", "yyyy-MM-dd HH:mm:ss");
+		long time = date.getTime();
+		Date date1 = DateUtils.getDate("2018-05-18 00:00:00", "yyyy-MM-dd HH:mm:ss");
+		long time1 = date1.getTime();
+		beanmap.put("start_time",time/1000 + "");
+		beanmap.put("end_time",time1/1000 + "");
+		beanmap.put("supplement_mode",entity.getSupplement_mode());
+		beanmap.put("sort_type","asc");
+		beanmap.put("coord_type_output","bd09ll");
+		beanmap.put("process_option",entity.getProcess_option());
+		beanmap.put("is_processed",entity.getIs_processed());
+		String postJson = null;
+
+		try {
+			postJson = HttpUtil.get(url, beanmap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		TrackPointsForReturn addPointResponse = JsonUtil.toBean(postJson,TrackPointsForReturn.class);
+
+		return addPointResponse;
+
+
+	}
 
 
 
