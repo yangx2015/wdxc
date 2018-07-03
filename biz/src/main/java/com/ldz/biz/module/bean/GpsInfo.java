d79618ca7e922309a57aded8cb9baf7ee3228cb1
chenwei <chenwei@gongyingshi.com>
2018-07-03 20:42:19 +0800

    用户机构

--- biz/src/main/java/com/ldz/biz/module/bean/GpsInfo.java
+++ biz/src/main/java/com/ldz/biz/module/bean/GpsInfo.java
@@ -30,13 +30,14 @@ public class GpsInfo {
 	private String fileSize;//文件大小(上传视频或者图片才会使用的参数)
 	private String filePostfix;//文件后缀（可以用于文件类型）
 	private String taskId;//任务id（用于上传服务器下载命令之后，终端上传之后回调给服务器之后的数据使用）
-	private String cmdType;//命令类型（推送消息时使用）  01：超速设定 02:灵敏度设定(急加速灵敏度)  
+	private String cmdType;//命令类型（推送消息时使用）  01：超速设定 02:灵敏度设定(急加速灵敏度)
+	private Boolean pushAll;
 	//11:拍（当前时间前后10s）视频 12:拍图片(实时) 13 合并视频   20 碰撞灵敏度     40 GPS心跳间隔   50上传视屏模式
 	/**  90:下发apk更新命令  91:设置上报数据的url   92:下发播报语音
      cmdType=01
       cmd值（ 1~150之间的数字)
-	
-    cmdType 02:  
+
+    cmdType 02:
 	    设置加减速监测灵敏等级，level 1-6，默认为 2 :
        1. 速度从零加到百公里约58秒内，能够被检测到急加速;
        2. 速度从零加到百公里约29秒内，能够被检测到急加速;
@@ -44,16 +45,16 @@ public class GpsInfo {
        4. 速度从零加到百公里约14秒内，能够被检测到急加速;
        5. 速度从零加到百公里约11秒内，能够被检测到急加速;
        6. 速度从零加到百公里约9秒内，能够被检测到急加速
-     
+
      cmdType=13
                   合并视频只需要将startTime 2017-10-12 00:00:00 和endTime 2017-10-12 00:00:00赋值即可
-      
+
      cmdType=20
        cmd值（ 00：低
         10：中
         20： 高)
-     
-   
+
+
      cmdType=40:
      	cmd值（ 1~300s之间的数字)
 
@@ -66,9 +67,9 @@ public class GpsInfo {
 	 * 摄像头参数如下:0,前后都抓拍, 1表示仅前摄像头, 2表示仅仅后摄像头。当cmdType为12的时候，此参数也是一样，只是抓拍前后多少秒参数无效【客户端自动判断，后台传递参数即可】
 	 * cmdType 为13的时候参数是0-0 或者1-0  ，特别注意，为13的时候，startTime和endTime必须有值
 	 * 摄像头参数如下:0 合并前摄像头  1 合并后摄像头  2 合并内置摄像头【内置摄像头这个暂时无法使用】
-	 * 
+	 *
 	 * cmdType为11 ，12 ，13的时候这样设定     值用cmdParams 里面
-       
+
       90:
        cmd:  apk具体下载的url
       91:
@@ -78,7 +79,7 @@ public class GpsInfo {
        cmd: 具体语音内容
 	 */
 	private String sczt; //该点位的上传状态  10点火上传  20熄火上传
-	private String cmd;//具体命令（推送消息时使用）   
+	private String cmd;//具体命令（推送消息时使用）
 	private String cmdParams;//其它参数（推送消息时使用）
 	private String fxj;//方向角
 	private String gpsjd;//GPS精度
