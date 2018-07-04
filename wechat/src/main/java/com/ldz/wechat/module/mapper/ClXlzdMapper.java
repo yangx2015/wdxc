6a2de4aa85068fdee6cc893e41454d5256a304d1
chenwei <libinbin@rhtop.cn>
2018-07-04 18:31:25 +0800

    车辆监控

--- wechat/src/main/java/com/ldz/wechat/module/mapper/ClXlzdMapper.java
+++ wechat/src/main/java/com/ldz/wechat/module/mapper/ClXlzdMapper.java
@@ -1,11 +1,8 @@
 package com.ldz.wechat.module.mapper;
 
-import com.ldz.util.cache.MybatisRedisCache;
-import com.ldz.util.mapperprovider.OracleInsertListMapper;
 import com.ldz.wechat.module.model.ClXlzd;
-import org.apache.ibatis.annotations.CacheNamespace;
-import org.apache.ibatis.cache.decorators.FifoCache;
 import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.special.InsertListMapper;
 
-public interface ClXlzdMapper extends Mapper<ClXlzd>,OracleInsertListMapper<ClXlzd> {
+public interface ClXlzdMapper extends Mapper<ClXlzd>,InsertListMapper<ClXlzd> {
 }
