c344bc3eec15e0819ec1b842201bcd93672db261
chenwei <chenwei@gongyingshi.com>
2018-07-31 21:03:41 +0800

    Merge branch 'master' of /develop/workspace/wdxc-new with conflicts.

--- web/pc/projectWD/src/axios/index.js
+++ web/pc/projectWD/src/axios/index.js
@@ -1,20 +1,20 @@
+import Vue from 'vue';
 import axios from 'axios'
+import api from './api'
 import Cookies from 'js-cookie';
 import {router} from '../router/index';
 import qs from 'qs';
 import store from '@/store'
+import env from '../../build/env';
 //订单分配权限
 
 // let url = 'http://47.98.39.45:8080'; // 生产环境(测试时不要改动这里)
- let url = 'http://47.98.39.45:8080'; //
- // let url = 'http://127.0.0.1:8082'; //
 // let url = 'http://47.98.39.45:8080'; //
-//  let url = 'http://127.0.0.1:80'; //
+let url = 'http://127.0.0.1:8080'; //
 // let url = 'http://localhost:8081'; // cw-mac
 // let url = 'http://192.168.31.181:80',//陈
 // let url = 'http://192.168.31.228:8088',//羊
 // let url = 'http://192.168.31.228:80';//羊
-// let url = 'http://192.168.3.234:80' // 宋
 let httpInstance = axios.create({
 baseURL: url,
     timeout: 300000,
@@ -51,9 +51,9 @@ httpInstance.interceptors.request.use((config) => {
         }
     }
     // 在发送请求之前做些什么
-    if(Cookies.get('accessToken')){
-        let accessToken = JSON.parse(Cookies.get('accessToken'));
-        config.headers.token = accessToken.token;
+    if(Cookies.get('result')){
+    	let accessToken = JSON.parse(Cookies.get('result')).accessToken;
+    	config.headers.token = accessToken.token;
     	config.headers.userid = accessToken.userId;
     }
     return config;
