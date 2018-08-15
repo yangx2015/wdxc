3f04af126146538816d33aaa959439fcbc1a3197
yaingx <udjpyud@163.com>
2018-08-15 18:10:48 +0800

    列驾驶员订单  列表 订单查询

---
+++ wechat/src/main/java/com/ldz/wechat/module/service/SjDdService.java
@@ -0,0 +1,12 @@
+package com.ldz.wechat.module.service;
+
+import com.ldz.util.bean.ApiResponse;
+import com.ldz.wechat.base.BaseService;
+import com.ldz.wechat.module.model.ClDd;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SjDdService extends BaseService<ClDd,String> {
+
+}
