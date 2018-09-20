a51240e13fde9e6b2fe23075e76051292d0b7eca
zhaohu <965567396@qq.com>
2018-09-21 02:31:04 +0800

    driv

--- web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/Consult/hisList.vue
+++ web/pc/projectWD/src/views/whdx/VehicleScheduling/OrderManagement/Consult/hisList.vue
@@ -1,5 +1,5 @@
 <template>
-      <div :id="mapID" style="height: 300px;background-color: #f5a623">
+      <div :id="mapID" style="height: 400px;width:100%;background-color: #f5a623">
       </div>
 </template>
 
@@ -15,19 +15,24 @@
                 mapID:'allmap',
                 map: '',
                 mapcenter: {
-                    lng: 114.368383,
-                    lat: 30.551134
+                    // lng: 114.368383,
+                    // lat: 30.551134
+                    lng: 113.888383,
+                    lat: 30.681134
                 },
                 zoom: 12,
             }
         },
         created(){
-            console.log(this.gpsList);
-            console.log(this.index);
+            // console.log(this.gpsList);
+            // console.log(this.index);
             this.mapID = this.mapID +this.index
         },
         mounted(){
-            this.showMap();
+            var v = this
+            setTimeout(function () {
+                v.showMap();
+            },50)
         },
         methods:{
             showMap() {
@@ -35,14 +40,17 @@
                 var v = this
                 // 百度地图API功能
                 this.map = new BMap.Map(v.mapID);    // 创建Map实例
-                if (this.gpsList.length > 0) {
-                    let lon = (this.gpsList[0].longitude+this.gpsList[this.gpsList.length-1].longitude)/2
-                    let lat = (this.gpsList[0].latitude+this.gpsList[this.gpsList.length-1].latitude)/2
-                    this.map.centerAndZoom(new BMap.Point(lon, lat), this.zoom);  // 初始化地图,设置中心点坐标和地图级别
-                } else {
-                    this.map.centerAndZoom(new BMap.Point(114.367161, 30.543013), this.zoom);  // 初始化地图,设置中心点坐标和地图级别
-
-                }
+                // if (this.gpsList.length > 0) {
+                //     let lng = (this.gpsList[0].longitude+this.gpsList[this.gpsList.length-1].longitude)/2
+                //     let lat = (this.gpsList[0].latitude+this.gpsList[this.gpsList.length-1].latitude)/2
+                let lng = this.gpsList[0].longitude
+                let lat = this.gpsList[0].latitude
+                    this.map.centerAndZoom(new BMap.Point(v.mapcenter.lng, v.mapcenter.lat), this.zoom);  // 初始化地图,设置中心点坐标和地图级别
+                // this.map.centerAndZoom(new BMap.Point(lng,lat), this.zoom)
+                // } else {
+                //     this.map.centerAndZoom(new BMap.Point(114.367161, 30.543013), this.zoom);  // 初始化地图,设置中心点坐标和地图级别
+                //
+                // }
                 //添加地图类型控件
                 this.map.addControl(new BMap.MapTypeControl({
                         mapTypes: [BMAP_NORMAL_MAP]
@@ -53,14 +61,13 @@
                 this.map.addControl(new BMap.OverviewMapControl());              //添加缩略地图控件
                 this.map.addControl(new BMap.NavigationControl());               // 添加平移缩放控件
 
-                // this.showStations(this.stationList)
-                this.showLine(this.gpsList)
                 this.addMarker(this.gpsList[0].longitude,
                     this.gpsList[0].latitude,
                     this.apis.STATIC_PATH+'icon/map_line_begin.png')
                 this.addMarker(this.gpsList[this.gpsList.length-1].longitude,
                     this.gpsList[this.gpsList.length-1].latitude,
                     this.apis.STATIC_PATH+'icon/map_line_end.png')
+                this.showLine(this.gpsList)
                 // setTimeout(function () {
                 //     v.map.setViewport(this.gpsList);
                 // },100)
