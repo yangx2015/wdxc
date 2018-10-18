4871e3f0df724825c98c8cda8fdf974f9ab294b0
yaingx <udjpyud@163.com>
2018-10-18 19:06:59 +0800

    增加日志

--- job/src/main/java/com/ldz/job/mapper/ClDdMapper.java
+++ job/src/main/java/com/ldz/job/mapper/ClDdMapper.java
@@ -6,7 +6,7 @@ import tk.mybatis.mapper.common.Mapper;
 
 public interface ClDdMapper extends Mapper<ClDd> {
     @Update("<script>" +
-            " UPDATE CL_DD D SET D.DDZT='12',D.BHYY='约车时间已过期，系统自动驳回用车申请' WHERE D.DDZT IN ('10','11') AND <![CDATA[ (D.YYSJ < (SYSDATE-1)) ]]>  " +
+            " UPDATE CL_DD D SET D.DDZT='12',D.BHYY='约车时间已过期，系统自动驳回用车申请'||to_char(sysdate,'yyyy-MM-dd HH24:mi:ss') WHERE D.DDZT IN ('10','11') AND <![CDATA[ (D.YYSJ < (SYSDATE-1)) ]]>  " +
             "</script>")
     void updateBeOverdueOrder();
 }
