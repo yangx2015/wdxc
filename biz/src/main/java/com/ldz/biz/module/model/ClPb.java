749a3fe8b0149c759a2cfae9125a6260d03bbeca
chenwei <chenwei@gongyingshi.com>
2018-10-30 22:44:51 +0800

    车辆排版

--- biz/src/main/java/com/ldz/biz/module/model/ClPb.java
+++ biz/src/main/java/com/ldz/biz/module/model/ClPb.java
@@ -103,6 +103,8 @@ public class ClPb implements Serializable {
      */
     @Column(name = "END_TIME")
     private String endTime;
+    @Column(name = "ENABLE")
+    private String enable;
     @Transient
     private String date;
 
@@ -122,6 +124,14 @@ public class ClPb implements Serializable {
     private String jssj;
 
     private static final long serialVersionUID = 1L;
+
+    public String getEnable() {
+        return enable;
+    }
+
+    public void setEnable(String enable) {
+        this.enable = enable;
+    }
 
     public String getClasses() {
         return classes;
